7ff7d75a5c387ef0bf153246e506610685b56842
oracleofammon <ydamiri@gmail.com>
2013-11-22 18:14:13 +0000

    skeleton files of demo

---
+++ web.js
@@ -0,0 +1,19 @@
+
+var express = require("express") 
+,   fs      = require("fs") 
+,   http    = require("http") 
+,   https   = require("https")
+
+var app = express(); 
+app.set("port", process.env.PORT || 8080); 
+
+app.get("/", function(request, response) { 
+  var data = fs.readFileSync("index.html").toString();
+  response.send(data); 
+});
+
+app.use(express.static(__dirname));
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log("Listening on " + app.get("port")); 
+}); 

67ac6c8659a236cebcfc81dae24c58704fc69f26
oracleofammon <ydamiri@gmail.com>
2013-11-26 16:46:04 +0000

    added bare bones of html

--- web.js
+++ web.js
@@ -1,19 +1,20 @@
 
-var express = require("express") 
-,   fs      = require("fs") 
-,   http    = require("http") 
-,   https   = require("https")
+var express = require('express')
+, fs      = require('fs')
+, http    = require('http')
+, https   = require('https')
 
-var app = express(); 
-app.set("port", process.env.PORT || 8080); 
+var app = express();
+app.set('port', process.env.PORT || 8080);
 
-app.get("/", function(request, response) { 
-  var data = fs.readFileSync("index.html").toString();
-  response.send(data); 
+
+app.get('/', function(request, response) {
+    var data = fs.readFileSync('index.html').toString();
+    response.send(data);
 });
 
 app.use(express.static(__dirname));
 
 http.createServer(app).listen(app.get('port'), function() {
-  console.log("Listening on " + app.get("port")); 
+    console.log("Listening on " + app.get('port'));
 }); 
